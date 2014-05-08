9a96fb258653e070f4421ef869f91ab71010942f
JasonRenner24 <jrenner15@my.whitworth.edu>
2014-05-08 10:05:41 -0700

    Added Program Counter

--- DataPathUnit.vhd
+++ DataPathUnit.vhd
@@ -83,13 +83,20 @@ architecture Behavioral of DataPathUnit is
 			 y:    out STD_LOGIC_VECTOR(width-1 downto 0));
 	end component adder;
 	
+	component pc is -- program counter
+		port(clk_in: 	 in std_logic;
+			  pc_in:      in  STD_LOGIC_VECTOR(15 downto 0);
+			  pc_out:      out STD_LOGIC_VECTOR(15 downto 0));
+	end component pc;
+	
 	component imem is -- instruction memory
 	port(a:  in  STD_LOGIC_VECTOR(15 downto 0);
        rd: out STD_LOGIC_VECTOR(15 downto 0));
 	end component;
 	
+	signal pc_connector: std_logic_vector(15 downto 0);
 	signal imm,srcA,srcB,writeData,memData,result: std_logic_vector(7 downto 0);
-	signal pc,pcPlusOne,pcJump,ZeroExtImm, iMemOut: std_logic_vector(15 downto 0);
+	signal next_pc,pcPlusOne,pcJump,ZeroExtImm, iMemOut: std_logic_vector(15 downto 0);
 	signal addr1,addr2:std_logic_vector(4 downto 0);
 	signal aluControl: std_logic_vector(2 downto 0);
 begin
@@ -110,7 +117,7 @@ begin
 	generic map(16)
 	port map
 	(
-		a => pc,
+		a => pc_connector,
 		b => "0000000000000001",
 		y => pcPlusOne
 	);
@@ -129,7 +136,7 @@ begin
 		d0 => pcPlusOne,
 		d1 => pcJump,
 		s  => PCSRC,
-		y  => pc
+		y  => next_pc
 	);
 	dec: Decoder 
 	port map 
@@ -165,9 +172,16 @@ begin
 	instrmem: imem
 	port map
 	(
-		a => "XXXXXXXXXXXXXXXX", -- address from regfile goes here
+		a => pc_connector, -- address from regfile goes here
 		rd => iMemOut
 	);
 	instr <= iMemOut;
+	PCounter:pc
+	port map
+	(
+		clk_in => clk,
+		pc_in => next_pc,
+		pc_out => pc_connector
+	);
 end Behavioral;
 
