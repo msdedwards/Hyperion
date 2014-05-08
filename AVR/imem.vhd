b829ddaf1ae81d710b297d86eafa8f5c44a7578f
mandella <nathan.ender@gmail.com>
2014-05-07 21:38:26 -0700

    Removed dmem

--- AVR/imem.vhd
+++ AVR/imem.vhd
@@ -8,26 +8,6 @@ use STD.TEXTIO.all;
 use IEEE.STD_LOGIC_UNSIGNED.all;  
 use IEEE.NUMERIC_STD.all;
 
-entity dmem is -- data memory
-  port(clk, we:  in STD_LOGIC;
-       a, wd:    in STD_LOGIC_VECTOR(31 downto 0);
-       rd:       out STD_LOGIC_VECTOR(31 downto 0));
-end;
-
-architecture behave of dmem is
-begin
-  process ( clk, a ) is
-    type ramtype is array (63 downto 0) of STD_LOGIC_VECTOR(31 downto 0);
-    variable mem: ramtype;
-  begin
-      if clk'event and clk = '1' then
-          if (we = '1') then mem( to_integer(unsigned(a(7 downto 2))) ) := wd;
-          end if;
-      end if;
-      rd <= mem( to_integer(unsigned(a(7 downto 2))) ); -- word aligned
-  end process;
-end;
-
 ------------------------------------------------------------------------------
 -- Instruction Memory
 ------------------------------------------------------------------------------
